8aa29e57ea3341ff3fbdc4a5d301bde8d0bbf719
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-02 09:57:36 -0300

    revat2

---
+++ revat2/exemplo1.js
@@ -0,0 +1,24 @@
+const leia = require("prompt-sync")()
+
+
+//let nomes = []
+
+//nomes[0] ="Giovanna" 
+//nomes[1] = 16
+//nomes[10] = {
+//    nome : "Vitória",
+//    idade : 16
+//}
+
+//console.log(nomes)
+//console.log(nomes[4])
+//console.log(nomes[10])
+//console.log(nomes.length)
+
+
+let mat = []
+mat[0 ="M01"]
+mat[1 ="M02"]
+mat[2 ="M03"]
+
+let nomes = ["Marcos","Pedro", "Maria"]
